23d8deeeb4af9757d134b39497c7b26d9743fab7
Eirik Ogilvie-Wigley <eowigley@gmail.com>
2018-11-10 23:46:54 -0700

    Incorporate game-template updates

--- src/pmn/main/PascalMain.java
+++ src/pmn/main/PascalMain.java
@@ -8,8 +8,9 @@ import gt.component.ComponentCreator;
 import gt.component.GameImage;
 import gt.component.GamePanel;
 import gt.component.MainFrame;
-import gt.drawable.GameState;
-import gt.drawable.UserInput;
+import gt.gamestate.GameState;
+import gt.gamestate.GameStateManager;
+import gt.gamestate.UserInput;
 
 public class PascalMain {
     private static final String TITLE = "Pascal's Triangle (mod n)";
@@ -17,12 +18,15 @@ public class PascalMain {
     public static void main(String[] args) {
         ComponentCreator.setCrossPlatformLookAndFeel();
 
-        GamePanel mainPanel = new GamePanel("Pascal", new PascalGameState());
+        GamePanel mainPanel = new GamePanel("Pascal");
         mainPanel.setPreferredSize(new Dimension(ComponentCreator.DEFAULT_WIDTH, ComponentCreator.DEFAULT_HEIGHT));
+
+        GameStateManager.setMainPanel(mainPanel);
+        GameStateManager.setGameState(new PascalGameState());
 
         MainFrame mainFrame = new MainFrame(TITLE, mainPanel);
 
-        mainFrame.show(mainPanel::addToGameLoop);
+        mainFrame.show();
     }
 
     private static class PascalGameState implements GameState {
@@ -31,48 +35,35 @@ public class PascalMain {
 
         private GameImage triangleImage = new GameImage();
 
-        private int width = ComponentCreator.DEFAULT_WIDTH;
-        private int height = ComponentCreator.DEFAULT_HEIGHT;
+        private int width;
+        private int height;
+
+        @Override
+        public void update(double dt) {
+            // Do nothing
+        }
 
         @Override
         public void drawOn(Graphics2D graphics) {
             graphics.drawImage(triangleImage.getImage(), 0, 0, null);
         }
 
-        private void redrawImage() {
-            GameImage triangleImageNew = new GameImage();
-            triangleImageNew.checkResized(width, height);
-            Graphics2D graphics = triangleImageNew.getGraphics();
-            graphics.setColor(ComponentCreator.backgroundColor());
-            graphics.fillRect(0, 0, width, height);
-            graphics.setColor(ComponentCreator.foregroundColor());
-            int modTemp = mod;
-            graphics.drawString("n = " + modTemp, 10, 20);
-            for (int y = 0; y < pascalsTriangle.length; ++y) {
-                int[] row = pascalsTriangle[y];
-                int rowLength = row.length;
-                for (int x = 0; x < rowLength; ++x) {
-                    int n = row[x];
-                    int xCoord = (width - rowLength) / 2 + x;
-                    graphics.setColor(getColor(n));
-                    graphics.drawLine(xCoord, y, xCoord, y);
-                }
-            }
-            triangleImage = triangleImageNew;
-        }
-
-        private static Color getColor(int nMod) {
-            if (nMod == 0) {
-                return Color.GREEN;
-            }
-            return Color.BLACK;
-        }
-
         @Override
-        public void componentResized(int width, int height) {
+        public void setSize(int width, int height) {
             this.width = width;
             this.height = height;
             recreateTriangle();
+        }
+
+        @Override
+        public void handleUserInput(UserInput input) {
+            if (input == UserInput.LEFT_BUTTON_RELEASED) {
+                ++mod;
+                recreateTriangle();
+            } else if (input == UserInput.RIGHT_BUTTON_RELEASED && mod > 2) {
+                --mod;
+                recreateTriangle();
+            }
         }
 
         private void recreateTriangle() {
@@ -101,15 +92,33 @@ public class PascalMain {
             redrawImage();
         }
 
-        @Override
-        public void handleUserInput(UserInput input) {
-            if (input == UserInput.LEFT_BUTTON_RELEASED) {
-                ++mod;
-                recreateTriangle();
-            } else if (input == UserInput.RIGHT_BUTTON_RELEASED && mod > 2) {
-                --mod;
-                recreateTriangle();
+        private void redrawImage() {
+            GameImage triangleImageNew = new GameImage();
+            triangleImageNew.setSize(width, height);
+            Graphics2D graphics = triangleImageNew.getGraphics();
+            graphics.setColor(ComponentCreator.backgroundColor());
+            graphics.fillRect(0, 0, width, height);
+            graphics.setColor(ComponentCreator.foregroundColor());
+            int modTemp = mod;
+            graphics.drawString("n = " + modTemp, 10, 20);
+            for (int y = 0; y < pascalsTriangle.length; ++y) {
+                int[] row = pascalsTriangle[y];
+                int rowLength = row.length;
+                for (int x = 0; x < rowLength; ++x) {
+                    int n = row[x];
+                    int xCoord = (width - rowLength) / 2 + x;
+                    graphics.setColor(getColor(n));
+                    drawPixel(graphics, xCoord, y);
+                }
             }
+            triangleImage = triangleImageNew;
+        }
+
+        private static Color getColor(int nMod) {
+            if (nMod == 0) {
+                return Color.GREEN;
+            }
+            return Color.BLACK;
         }
     }
 }
